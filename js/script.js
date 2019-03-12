36ec2bc033f3f0845eea5aef283c3edb29842978
Cwell21 <cwe11@yahoo.com>
2019-03-11 19:16:15 -0600

    Slide show functional.  Need to fine tune - Timing of slide change, speed of image load, ect

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@
 
 (function() {
 
+  const slide = () => {
+    let slideCounter = 0;
+    const el = document.getElementById("slide")
 
+  return () => {
+      if (slideCounter <= 3 && slideCounter > 0) {
+        el.style.backgroundImage = `url(./images/backgroundImages/bg${slideCounter}.jpg)`;
+        slideCounter += 1;
+      }else {
+        slideCounter = 1;
+      }
+    }
+  }
+
+  setInterval(slide(), 5000);
 
 }());
